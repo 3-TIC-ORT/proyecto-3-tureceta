b0b2f994e1b826ddade36b2dfc2d4093ad45487c
benjaiu10 <benja.iucht@gmail.com>
2024-08-07 10:31:29 -0300

    prueba js registro

--- seguundo js benja.js
+++ seguundo js benja.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function() {
     let nombre = localStorage.getItem("nombre");
     if (nombre) {
-        document.getElementById("mostrarNombre").textContent = "Manu es:"  + nombre;
+        document.getElementById("mostrarNombre").textContent = "Hola"  + nombre;
     } else {
         document.getElementById("mostrarNombre").textContent = "No se encontró el nombre.";
     }
