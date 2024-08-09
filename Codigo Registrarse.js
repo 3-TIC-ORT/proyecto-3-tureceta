274611a240d1e8af4e3cd6e871f4fcdc8a37a47f
benjaiu10 <benja.iucht@gmail.com>
2024-08-09 08:03:09 -0300

    cambios nombres

--- Codigo Registrarse.js
+++ Codigo Registrarse.js
@@ -3,6 +3,18 @@ document.getElementById("BotonRegistro").addEventListener("click", function() {
 let nombre = document.getElementById("nombre").value;
 let contraseña = document.getElementById("contraseña").value
 let correo = document.getElementById("correo").value
+//lo siguiente hara que el nombre y la contraseña tengan un minimo de caracteres
+if (nombre.length < 1){
+    alert ("El Nombre debe tener mas de un caracter")
+    return
+    
+}
+if (contraseña.length < 1){
+    alert ("La contraseña debe tener mas de un caracter")
+    return
+    
+}
+
 // Crear objeto para cada usuario con usuario y contraseña
 let usuario = {
 nombre: nombre,
@@ -15,6 +27,6 @@ localStorage.setItem(correo, JSON.stringify(usuario));
 // Limpiar los campos de entrada
 document.getElementById("nombre").value = '';
 document.getElementById("contraseña").value = '';
-window.location.href = "";
+window.location.href = "Paginascroll1.html";
 });
 
