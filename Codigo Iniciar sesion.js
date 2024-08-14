b1bc460c844bda64a44d6241e178bb1563bf462d
benjaiu10 <benja.iucht@gmail.com>
2024-08-14 15:20:17 -0300

    login

--- Codigo Iniciar sesion.js
+++ Codigo Iniciar sesion.js
@@ -1,2 +1,14 @@
+document.getElementById("BotonLogin").addEventListener("click", function(){
+    let contraseña = document.getElementById("contraseña").value
+    let correo = document.getElementById("correo").value
+    if (correo.lenght < 1){
+        alert ("el correo debe tener mas de un caracter")
+    return
+    }
+
+    if (contraseña.lenght < 1){
+        alert ("La contraseña debe tener mas de un caracter")
+    }
+})
 
     
