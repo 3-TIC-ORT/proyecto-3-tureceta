03bf40d9bb09e400d5c8ba68d14f4f3b5981cf1c
benjaiu10 <benja.iucht@gmail.com>
2024-08-23 15:48:08 -0300

    cree el arhicov jason y logre que lo que se ponga en el registro se guarde en un archivo json

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 import fs from "fs"
-let usuario = {
-    nombre: nombre,
-    contraseña: contraseña,
 
-};
-fs.writeFileSync("usuario.json",JSON.stringify(usuario))
+function registrarse(nombre, correo, contraseña){
+    let usuarios = fs.readFileSync("usuario.json","utf-8");
+    usuarios = JSON.parse(usuarios);
+    if (nombre.length < 1){
+        alert ("El Nombre debe tener mas de un caracter")
+        return
+        
+    }
+    if (contraseña.length < 1){
+        alert ("La contraseña debe tener mas de un caracter")
+        return
+        
+    }
+    
+    if (correo.length < 1){
+        alert ("El correo debe tener mas de un caracter")
+        return
+        
+    }
+    //esto detecta si el correo contiene un dominio valido
+    if (
+        (correo.includes("@gmail.com") || correo.includes("@hotmail.com") || correo.includes("@est.ort.edu.ar")) &&correo.indexOf("@") > 0 && correo.endsWith(".com")) {
+        // Crear objeto para cada usuario con nombre y contraseña
+        let usuario = {
+            nombre: nombre,
+            contraseña: contraseña,
+            correo: correo,
+        };
+        
+
+        usuarios.push(usuario);
+        // Guardar la lista de usuarios en usuario.json
+        fs.writeFileSync("usuario.json",JSON.stringify(usuarios));
+    
+    } else {
+        // Mostrar un mensaje de error si el correo no es válido
+        alert("Por favor ingrese un correo válido");
+    }
+}
+
+registrarse("leoo","hola@gmail.com","1234");
