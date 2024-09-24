d092294b3622d0b17a72c17a6f452155051976d0
benjaiu10 <benja.iucht@gmail.com>
2024-09-24 08:23:17 -0300

    iniiicar sesion

--- Back/FronPruebLogIn.js
+++ Back/FronPruebLogIn.js
@@ -1,5 +1,4 @@
 let botonLogin = document.getElementById("botonLogin")
-
 function enviar(){
 let correo = document.getElementById("correo").value
 let contraseña = document.getElementById("contraseña").value
@@ -8,15 +7,17 @@ let usuario = {
     correo: correo,
     contraseña: contraseña,
 };
- postData("login",usuario,function(ok){
 
-    if(ok===true){
-    window.location.href = "http://127.0.0.1:5500/Front/Paginascroll1.html";
-} else {
-    alert("Correo o contraseña incorrectos");
-}
+// Llamar a la función postData y manejar la respuesta del backend
+postData("login", usuario, function(ok) {
+    if (ok === true) {
+        // Redirigir si el login es exitoso
+        window.location.href = "http://127.0.0.1:5500/Front/Paginascroll1.html";
+    } else {
+        console.log("Login fallido");
+        alert("Correo o contraseña incorrectos");
+    }
 });
 }
-
 botonLogin.addEventListener("click", enviar)
 
---
+++ Back/LogIn.js
@@ -0,0 +1,29 @@
+import fs from "fs"
+import {onEvent,sendEvent,startServer, } from "soquetic";
+// Leer la lista de usuarios del archivo JSON    
+let usuarios = fs.readFileSync("usuarios.json","utf-8");
+usuarios = JSON.parse(usuarios);
+
+function loginBack(data){
+    let ok = false;
+
+    for (let i = 0; i<usuarios.length; i++){
+        if (usuarios[i].correo === data.correo && usuarios[i].contraseña === data.contraseña){
+            console.log("Bienvenido, " + usuarios[i].nombre);
+            ok = true; // Credenciales correctas
+            break; // Detener el bucle si encuentra coincidencia
+        }
+        if (!ok) {
+            console.log("Correo o contraseña incorrectos");
+        }
+
+        sendEvent("loginOK",ok);
+}
+
+}
+
+
+onEvent("login",loginBack);
+
+startServer()
+
